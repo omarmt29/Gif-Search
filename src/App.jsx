d0a0fb6327c7791f75b822abd7641affbbbf728f
omarmt29 <omarmendezt29@gmail.com>
2023-12-01 12:26:25 -0400

    fasfsffffff

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ function App() {
   const [gifData, setGifData] = useState({ url: Url, apiKey: apiKey, query: 'gatitos', limit: 3 });
 
   const handleFetchGifs = async (query, limit) => {
-   
+
     try {
       const res = await fetch(`${Url}?api_key=${apiKey}&q=${query}&limit=${limit}`)
-      const {data} = await res.json();
+      const { data } = await res.json();
       setGifs(data)
     } catch (error) {
       console.log(error);
@@ -23,27 +23,29 @@ function App() {
   useEffect(() => {
     handleFetchGifs(gifData.query, gifData.limit);
   }, [gifData.query, gifData.limit]);
- 
+
   return (
     <div>
-      <div className='m-auto flex w-56 gap-2 fixed top-0 mt-2'>
-        <div className='relative w-56 '>
+      <div className="flex justify-center relative">
+        <div className='m-auto flex w-56 gap-2 fixed bottom-5 mt-2'>
+          <div className='relative w-56 '>
+            <input
+              onChange={(e) => setGifData({ ...gifData, query: e.target.value })}
+              className='shadow-none outline-none rounded-2xl px-7 w-full py-1 text-xl'
+              type='text'
+              placeholder='Search Gif'
+            />
+            <FaSistrix className='absolute ml-2 bottom-2  text-black/60' />
+          </div>
           <input
-            onChange={(e) => setGifData({ ...gifData, query: e.target.value })}
-            className='shadow-none outline-none rounded-2xl px-6 w-full py-1 text-sm'
-            type='text'
-            placeholder='Search'
+            onChange={(e) => setGifData({ ...gifData, limit: e.target.value })}
+            type='number'
+            className='w-16 rounded-xl text-center text-xl'
+            placeholder='Limit'
           />
-          <FaSistrix className='absolute ml-1 bottom-0 mb-[6px] text-black/20' />
         </div>
-        <input
-          onChange={(e) => setGifData({ ...gifData, limit: e.target.value })}
-          type='number'
-          className='w-16 rounded-xl text-center text-sm'
-          placeholder='Limit'
-        />
       </div>
-      <div className='grid grid-cols-12 mt-6'>
+      <div className='grid grid-cols-12 mt-6 pb-16'>
         {gifs.length > 0 ? (
           gifs.map((gif) => (
             <div key={gif.id} className='col-span-4 sm:col-span-3'>
